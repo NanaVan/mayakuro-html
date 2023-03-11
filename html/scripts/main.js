d178f20ecc83debcef2db4f8abf3bd101cb7ed5a
NanaVan <van4wang@hotmail.com>
2023-03-12 00:41:14 +0800

    update HTML files

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -21,8 +21,7 @@ function A1Btn() {
 function A2Btn() {
 	if(document.getElementById('A2Input').value =='やまと'){
 		document.getElementById('Q2Blank').innerHTML = 'やまと';
-		document.getElementById('A2Btn').style.display = 'none';	
-		document.getElementById('A2Input').style.display = 'none';	
+		document.getElementById('P2Btn').style.display = 'none';	
 		document.getElementById('P3').style.display = 'block';
 		document.getElementById('P3Btn').style.display = 'block';
 	}else{
@@ -34,9 +33,7 @@ function A3Btn() {
 	if(document.getElementById('A31Input').value =='クロード' && document.getElementById('A32Input').value =='ドビュッシー' ){
 		document.getElementById('Q31Blank').innerHTML = 'クロード';
 		document.getElementById('Q32Blank').innerHTML = 'ドビュッシー';
-		document.getElementById('A3Btn').style.display = 'none';	
-		document.getElementById('A31Input').style.display = 'none';	
-		document.getElementById('A32Input').style.display = 'none';	
+		document.getElementById('P3Btn').style.display = 'none';	
 		document.getElementById('P4').style.display = 'block';
 		document.getElementById('P4Btn').style.display = 'block';
 	}else{
@@ -49,8 +46,7 @@ function A3Btn() {
 function A4Btn() {
 	if(document.getElementById('A4Input').value =='ファンシーポッズ'){
 		document.getElementById('Q4Blank').innerHTML = 'ファンシーポッズ';
-		document.getElementById('A4Btn').style.display = 'none';	
-		document.getElementById('A4Input').style.display = 'none';	
+		document.getElementById('P4Btn').style.display = 'none';	
 		document.getElementById('P5').style.display = 'block';
 		document.getElementById('P5Btn').style.display = 'block';
 	}else{
@@ -61,11 +57,32 @@ function A4Btn() {
 function A5Btn() {
 	if(document.getElementById('A5Input').value =='ユー'){
 		document.getElementById('Q5Blank').innerHTML = 'ユー';
-		document.getElementById('A5Btn').style.display = 'none';	
-		document.getElementById('A5Input').style.display = 'none';	
+		document.getElementById('P5Btn').style.display = 'none';	
 		document.getElementById('P6').style.display = 'block';
+		document.getElementById('P6Btn').style.display = 'block';
 	}else{
 		document.getElementById('A5Input').value="";	
 		document.getElementById('A5Input').setAttribute("placeholder", '错了！再来');	
 	}
+}
+function A6Btn() {
+	if(document.getElementById('A61Input').value =='まやクロ' && document.getElementById('A62Input').value =='ファンシー' && document.getElementById('A63Input').value =='ユー'){
+		document.getElementById('Q6Blank').innerHTML = 'まやクロのファンシー　ユー';
+		document.getElementById('P6Btn').style.display = 'none';	
+		document.getElementById('P7').style.display = 'block';
+		document.getElementById('P1').style.display = 'none';
+		document.getElementById('P2').style.display = 'none';
+		document.getElementById('P3').style.display = 'none';
+		document.getElementById('P4').style.display = 'none';
+		document.getElementById('P5').style.display = 'none';
+		document.getElementById('P6').style.display = 'none';
+	}else{
+		document.getElementById('A61Input').value="";	
+		document.getElementById('A62Input').value="";	
+		document.getElementById('A63Input').value="";	
+		document.getElementById('A62Input').setAttribute("placeholder", '错了！再来');	
+	}
+}
+function A7Btn() {
+	window.open("https://docs.google.com/forms/d/e/1FAIpQLSdC3XCSKdpLHPVYK7FyfqLEgzEF1oaqdvaHABCx6vic2WRG1w/viewform");
 }
